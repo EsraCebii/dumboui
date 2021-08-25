eeb7be9de709668ad91f25c0005f68c633e0fd77
Esra Cebi <esracebi.81@gmail.com>
2021-08-25 23:08:20 +0300

    buttons css

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
-import { ExampleComponent } from 'dumbo'
+import { ButtonComponent} from 'dumbo'
 import 'dumbo/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+
+    <ButtonComponent type="link" text="Click me !" />
+
+  )
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+
+export const ButtonComponent = (props) => {
+  console.log(props)
+  return(
+  <button className={styles[props.type]} >{props.text} </button>
+  )
 }
